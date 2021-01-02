47c7462ded49d97f535ebfacda077ed3c960fb6b
“saschakala” <mxsaschakala@gmail.com>
2021-01-02 11:58:05 -0500

    Done.

--- src/containers/App.js
+++ src/containers/App.js
@@ -22,7 +22,7 @@ class App extends Component {
         <div>
           <NavBar />
           <Route exact path="/" render={() => <div>Home</div>} />
-          <Route path='/movies' render={routerProps => <MoviesPage {...routerProps} movies={this.state.movies}/>} />
+<Route path='/movies' render={routerProps => <MoviesPage {...routerProps} movies={this.state.movies}/>} />
         </div>
       </Router>
     );
--- src/containers/MoviesPage.js
+++ src/containers/MoviesPage.js
@@ -1,12 +1,15 @@
-// .src/containers/MoviesPage.js
 import React from 'react';
 import { Route } from 'react-router-dom';
 import MoviesList from '../components/MoviesList';
-
+import MovieShow from '../components/MovieShow';
+ 
 const MoviesPage = ({ match, movies }) => (
   <div>
     <MoviesList movies={movies} />
+    {/* // Here we replace the `component` prop with the `render` prop so we can pass the 
+    // route information to the `MovieShow` component */}
+    <Route path={`${match.url}/:movieId`} render={routerProps => <MovieShow {...routerProps} movies={movies} /> }/>
   </div>
 )
-
+ 
 export default MoviesPage
